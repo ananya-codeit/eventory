7b345ece957dee01a37b8697bec0b33d82ea2149
ananya-codeit <aagarwal3_be20@thapar.edu>
2021-05-14 16:10:41 +0530

    Second commit

--- lib/signup1.dart
+++ lib/signup1.dart
@@ -12,95 +12,104 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 35.0),
-              child: Center(
-                child: Text(
-                  'Eventory',
-                  style: TextStyle(
-                    fontFamily: 'Mystical Snow',
-                    color: Colors.white,
-                    fontSize: 60.0,
-                    fontWeight: FontWeight.w500,
-                  ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical:60,horizontal:40),
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 35.0),
+                child: Center(
+                  child: Text(
+                    'Eventory',
+                    style: TextStyle(
+                      fontFamily: 'Mystical Snow',
+                      color: Colors.black,
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.w500,
+                    ),
 
+                  ),
                 ),
               ),
-            ),
-            Image(image: AssetImage('images/rafiki.png'),),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 6.0,horizontal: 100.0),
-              child: GestureDetector(
-                onTap: (){
-                  Navigator.pushNamed(context, SignUp.id);
-                },
-                child: Container(
-                  height: 50,
-                  width: 80,
-                  decoration: BoxDecoration(
-                    border: Border.all(color:Colors.black),
-                    borderRadius: BorderRadius.circular(30),
+              Image(image: AssetImage('images/rafiki.png'),),
+              SizedBox(
+                height:60.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical:10,horizontal:10),
+                child: GestureDetector(
+                  onTap: (){
+                    Navigator.pushNamed(context, SignUp.id);
+                  },
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(color:Colors.black),
+                      borderRadius: BorderRadius.circular(30),
 
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Sign Up as a Student',
-                      style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w500,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Sign Up as a Student',
+                        style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
+
+
                   ),
-
-
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 6.0,horizontal: 100.0),
-              child: GestureDetector(
-                onTap: (){
-                  print('yes');
-                },
-                child: Container(
-                  height: 50,
-                  width: 80,
-                  decoration: BoxDecoration(
-                    border: Border.all(color:Colors.black),
-                    borderRadius: BorderRadius.circular(30),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical:10,horizontal:10),
+                child: GestureDetector(
+                  onTap: (){
+                    print('yes');
+                  },
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(color:Colors.black),
+                      borderRadius: BorderRadius.circular(30),
 
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Sign Up as a Society',
-                      style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w500,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Sign Up as a Society',
+                        style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
+
+
                   ),
-
-
                 ),
               ),
+
+
+
+            ],
+          ),
+
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30.0),
+            border: Border.all(
+              color:Colors.black,
+
             ),
 
 
 
-          ],
-        ),
-
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30.0),
-          color: Colors.black,
-
-
-
+          ),
         ),
       ),
     );
